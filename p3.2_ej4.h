c88ae22847a53d09b521ddaf316c12545ed952dc
nnocetti <nnocetti@gmail.com>
2013-10-02 20:15:45 -0700

    Update p3.2_ej4.h

--- p3.2_ej4.h
+++ p3.2_ej4.h
@@ -1,35 +1,37 @@
 #ifndef _GRAFO_H
 #define _GRAFO_H
 
+#include "conjunto.h"
+
 struct repGrafo;
 typedef repGrafo* Grafo;
 typedef int Vertice;
-typedef int[2] Arista;
-struct repConjuntoV;
-typedef repConjuntoV* ConjuntoDeVertices; 
+struct repArista;
+typedef repArista Arista;
 
 Grafo CrearGrafo();
 //devuelve el grafo vacio
 
 void AgregarVertice(Grafo &g, Vertice v);
 //inserta el vertice v en el grafo g
-//Precondicion: o el vertice es 0 o existe el vertice v-1 en el grafo g
+//Precondicion1: no exite el vertice v en el grafo g
+//Precondicion2: o el vertice es 0 o existe el vertice v-1 en el grafo g
 
 void QuitarVertice(Grafo &g, Vertice v);
 //remueve el vertice v del grafo g
-//Precondicion: existe el vertice v en el grafo g
+//Precondicion: existe el vertice v en el grafo g y no existe el vertice v+1
 
 
 void AgregarArista(Grafo &g, Arista a);
 //inserta la arista a en el grafo g
-//Precondicion: existen los vertice a[0] y a[1]
+//Precondicion: existen los vertice a.v y a.w
 
 void QuitarArista(Grafo &g, Arista a);
 //Precondicion: existe la arista a en el grafo g
 
-ConjuntoDeVertices Vertices(Grafo g);
+ConjuntoInt Vertices(Grafo g);
 
-ConjuntoDeVertices Adyacentes(Grafo g, Vertice v);
+ConjuntoInt Adyacentes(Grafo g, Vertice v);
 
 void destruirGrafo(Grafo &g);
 // libera toda la memoria ocupada por g
